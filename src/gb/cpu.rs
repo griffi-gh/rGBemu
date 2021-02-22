9a8e5cf9b05505bef140914cdaac357b81783f52
griffi-gh <prasol258@gmail.com>
2021-02-22 13:05:02 +0200

    Update cpu.rs

--- src/gb/cpu.rs
+++ src/gb/cpu.rs
@@ -14,12 +14,22 @@ impl Cpu {
 		}
 	}
 
-	fn set_carry(&mut self, v: u8, a: u8) {
+	fn set_carry_add(&mut self, v: u8, a: u8) {
 		self.reg.f.c = !(v.checked_add(a).is_some());
 	}
-	fn set_flags(&mut self, v: u8, a: u8) {
+	fn set_flags_add(&mut self, v: u8, a: u8) {
 		self.reg.f.z = v == 0;
 		self.reg.f.h = ((v & 0xF) + (a & 0xF)) > 0xF;
+		self.reg.f.n = false;
+	}
+
+	fn set_carry_sub(&mut self, v: u8, a: u8) {
+		self.reg.f.c = !(v.checked_sub(a).is_some());
+	}
+	fn set_flags_sub(&mut self, v: u8, a: u8) {
+		self.reg.f.z = v == 0;
+		self.reg.f.h = ((v & 0xF) - (a & 0xF)) > 0xF;
+		self.reg.f.n = true;
 	}
 	
 	fn read_byte(&mut self, mem: &Memory) -> u8 {
@@ -44,6 +54,8 @@ impl Cpu {
 				//NOP
 				4
 			}
+
+			// LD rr,u16
 
 			0x01 => {
 				//LD BC,u16
@@ -80,9 +92,16 @@ impl Cpu {
 				8
 			}
 
+			// INC rr
+
 			0x03 => {
 				//INC BC
-				self.reg.set_bc(self.reg.get_de().wrapping_add(1));
+				self.reg.set_bc(self.reg.get_bc().wrapping_add(1));
+				8
+			}
+			0x13 => {
+				//INC DE
+				self.reg.set_de(self.reg.get_de().wrapping_add(1));
 				8
 			}
 			0x23 => {
@@ -96,51 +115,70 @@ impl Cpu {
 				8
 			}
 
+			// DEC rr
+
+			0x0B => {
+				//DEC BC
+				self.reg.set_bc(self.reg.get_bc().wrapping_add(1));
+				8
+			}
+			0x1B => {
+				//DEC DE
+				self.reg.set_de(self.reg.get_de().wrapping_add(1));
+				8
+			}
+			0x2B => {
+				//DEC HL
+				self.reg.set_hl(self.reg.get_hl().wrapping_add(1));
+				8
+			}
+			0x3B => {
+				//DEC SP
+				self.reg.sp = self.reg.sp.wrapping_add(1);
+				8
+			}
+
+			//INC r
+
 			0x04 => {
 				//INC B
 				let v: u8 = self.reg.b.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.b = v;
 				4
 			}
 			0x0C => {
 				//INC C
 				let v: u8 = self.reg.c.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.c = v;
 				4
 			}
 			0x14 => {
 				//INC D
 				let v: u8 = self.reg.d.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.d = v;
 				4
 			}
 			0x1C => {
 				//INC E
 				let v: u8 = self.reg.e.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.e = v;
 				4
 			}
 			0x24 => {
 				//INC H
 				let v: u8 = self.reg.h.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.h = v;
 				4
 			}
 			0x2C => {
 				//INC L
 				let v: u8 = self.reg.l.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				self.reg.l = v;
 				4
 			}
@@ -148,16 +186,74 @@ impl Cpu {
 				//INC (HL)
 				let a: u16 = self.reg.get_hl();
 				let v: u8 = mem.read(a).wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
 				mem.write(a, v);
 				12
 			}
 			0x3C => {
 				//INC A
 				let v: u8 = self.reg.a.wrapping_add(1);
-				self.set_flags(v, 1);
-				self.reg.f.n = false;
+				self.set_flags_add(v, 1);
+				self.reg.a = v;
+				4
+			}
+
+			// DEC r
+			
+			0x05 => {
+				//DEC B
+				let v: u8 = self.reg.b.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.b = v;
+				4
+			}
+			0x0D => {
+				//DEC C
+				let v: u8 = self.reg.c.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.c = v;
+				4
+			}
+			0x15 => {
+				//DEC D
+				let v: u8 = self.reg.d.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.d = v;
+				4
+			}
+			0x1D => {
+				//DEC E
+				let v: u8 = self.reg.e.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.e = v;
+				4
+			}
+			0x25 => {
+				//DEC H
+				let v: u8 = self.reg.h.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.h = v;
+				4
+			}
+			0x2D => {
+				//DEC L
+				let v: u8 = self.reg.l.wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				self.reg.l = v;
+				4
+			}
+			0x35 => {
+				//DEC (HL)
+				let a: u16 = self.reg.get_hl();
+				let v: u8 = mem.read(a).wrapping_sub(1);
+				self.set_flags_sub(v, 1);
+				mem.write(a, v);
+				12
+			}
+			0x3D => {
+				//DEC A
+				let v: u8 = self.reg.l.wrapping_sub(1);
+				self.set_flags_sub(v,1);
 				self.reg.a = v;
 				4
 			}
